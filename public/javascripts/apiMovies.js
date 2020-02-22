dd355f7c381a55268afc4907cb9c6b3b2c881a52
militroncho7 <victor@MacBook-Air-de-Victor.local>
2020-02-22 14:33:53 +0100

    Print Genre, Title & Languages

--- public/javascripts/apiMovies.js
+++ public/javascripts/apiMovies.js
@@ -1,5 +1,3 @@
-const gen = require("genres.js")
-
 //const baseURL = "https://api.themoviedb.org/3/discover/movie?api_key=62706964a54757878e9737ac5a8ceafc&with_genres=18&with_original_language=ca&sort_by=runtime.asc"
 const languages = document.querySelector("#language");
 const genres = document.querySelector("#genre");
@@ -7,54 +5,54 @@ const genres = document.querySelector("#genre");
 
 //&with_original_language=en
 
-// const gen = [{
-//   "id": 28,
-//   "name": "Action"
-// },
-// {
-//   "id": 12,
-//   "name": "Adventure"
-// },
-// {
-//   "id": 16,
-//   "name": "Animation"
-// },
-// {
-//   "id": 35,
-//   "name": "Comedy"
-// },
-// {
-//   "id": 80,
-//   "name": "Crime"
-// },
-// {
-//   "id": 18,
-//   "name": "Drama"
-// },
-// {
-//   "id": 14,
-//   "name": "Fantasy"
-// },
-// {
-//   "id": 27,
-//   "name": "Horror"
-// },
-// {
-//   "id": 9648,
-//   "name": "Mystery"
-// },
-// {
-//   "id": 10749,
-//   "name": "Romance"
-// },
-// {
-//   "id": 878,
-//   "name": "Science Fiction"
-// },
-// {
-//   "id": 53,
-//   "name": "Thriller"
-// }]
+const gen = [{
+  "id": 28,
+  "name": "Action"
+},
+{
+  "id": 12,
+  "name": "Adventure"
+},
+{
+  "id": 16,
+  "name": "Animation"
+},
+{
+  "id": 35,
+  "name": "Comedy"
+},
+{
+  "id": 80,
+  "name": "Crime"
+},
+{
+  "id": 18,
+  "name": "Drama"
+},
+{
+  "id": 14,
+  "name": "Fantasy"
+},
+{
+  "id": 27,
+  "name": "Horror"
+},
+{
+  "id": 9648,
+  "name": "Mystery"
+},
+{
+  "id": 10749,
+  "name": "Romance"
+},
+{
+  "id": 878,
+  "name": "Science Fiction"
+},
+{
+  "id": 53,
+  "name": "Thriller"
+}]
 
 
 const findGenres = genresToFind => gen.find(el => el.id == genresToFind).name
