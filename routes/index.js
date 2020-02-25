bf532e20acec73725993e8965426ca93b7a6866b
militroncho7 <victor@MacBook-Air-de-Victor.local>
2020-02-25 20:37:53 +0100

    Created routes & edit cards

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  return res.render("index");
 });
+
+
+router.get("/movie/:id", (req, res, next) => {
+  const movieId = req.params.id;
+  axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=62706964a54757878e9737ac5a8ceafc`)
+    .then((results) => {
+      return res.render("passport/card", { name: "original_title" });
+    })
+    .catch(err => console.log(err));
+})
+
+// router.get("/movie/:id", (req, res, next) => {
+//   const movieId = req.params.id;
+//   axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=62706964a54757878e9737ac5a8ceafc`)
+//     .then((results) => {
+//       return res.render("passport/card", results.data);
+//     })
+//     .catch(err => console.log(err));
+// })
 
 module.exports = router;
